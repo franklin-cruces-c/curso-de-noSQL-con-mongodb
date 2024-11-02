a4452505238c1c69488c604de0cf218461d0270b
franklin-cruces <franklin.cruces@hotmail.com>
2024-11-01 22:26:56 -0300

    Obtener documentos a partir de atributos de documentos embebidos utilizando dot notattion

--- sentenciasModulo3.js
+++ sentenciasModulo3.js
@@ -37,3 +37,41 @@ db.users.updateOne(
         }
     }
 )
+
+/**
+ * Obtener documentos a partir de atributos de documentos embebidos
+ */
+
+// Obtener todos los usuarios que posean una direccion postal
+db.users.find(
+    {address: {$exists: true}}
+).pretty()
+
+// Obtener todos los usuarios que posean un código postal 1 y un número igual a 10
+// Dot Notattion
+db.users.find(
+    {
+       $and:[
+        {'address.postalCode': 1},
+        {'address.number': 10}          
+       ]       
+    }
+).pretty()
+
+// Obtener el nombre y la primera referencia de todos los usuarios con direccion postal y referencias
+
+db.users.find(
+    {
+       $and:[
+        {
+          address: {$exists: true}
+        },
+        {
+          'address.references':{$exists: true}
+        }
+       ] 
+    },
+    {
+        _id: false, name: true, 'address.references' : {$slice : 1}
+    }
+).pretty()
