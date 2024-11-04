c7396e36b4f59ea39cf9b84cbe6d6fd4b1426c9d
franklin-cruces <franklin.cruces@hotmail.com>
2024-11-04 13:32:59 -0300

    se utiliza el framework aggregate con match, project, slice y arrayElemAt

--- sentenciasModulo4.js
+++ sentenciasModulo4.js
@@ -88,3 +88,77 @@ db.users.aggregate(
 
     ]
 ).pretty()
+
+/**
+ * Operador slice con framework aggregate
+ */
+
+
+// mostrar el nombre y los 2 primeros cursos de los usuarios que tengan mas de 25 años
+// y posean una lista de cursos y que tengan por apellido alvarez
+db.users.aggregate(
+    [
+        {
+            $match: {
+                age:{ $gt:25} // se reduce a 3 documentos
+            }        
+        },
+        {
+            $match: {
+                courses: {$exists: true} // se reduce a 2 documentos
+            }
+        },
+        {
+            $project: {
+                _id:false, name:true, courses:true
+            }
+        },
+        {
+           $project: {
+                name: true, 
+                firstCourse: {
+                    $slice:['$courses',2]
+                }
+           }
+        }
+    ]
+).pretty()
+
+// mostrar el nombre y el primer curso de los usuarios que tengan mas de 25 años
+// y posean una lista de cursos y que tengan por apellido alvarez
+db.users.aggregate(
+    [
+        {
+            $match: {
+                age:{ $gt:25} // se reduce a 3 documentos
+            }        
+        },
+        {
+            $match: {
+                courses: {$exists: true} // se reduce a 2 documentos
+            }
+        },
+        {
+            $project: {
+                _id:false, name:true, courses:true
+            }
+        },
+        {
+           $project: {
+                name: true, 
+                firstCourses: {
+                    $slice:['$courses',2]
+                }
+           }
+        },
+        {
+           $project: {
+                name: true, 
+                course: {
+                    $arrayElemAt:['$firstCourses',0]
+                }
+           }
+        }
+
+    ]
+).pretty()
