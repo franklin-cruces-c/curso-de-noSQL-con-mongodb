e35f4f9b7bfcaa66f8c71d9115fa40da8437ded7
franklin-cruces <franklin.cruces@hotmail.com>
2024-11-06 02:56:56 -0300

    Framework aggregate - Concatenar

--- sentenciasModulo4.js
+++ sentenciasModulo4.js
@@ -273,3 +273,36 @@ db.users.aggregate(
         }
     }
 )
+
+/**
+ * Concatenar atributos
+ */
+
+db.users.aggregate(
+    [
+        {
+            $match:{
+                $and:[
+                    {
+                        name:{$exists: true}
+                    },
+                    {
+                        lastName: {$exists: true}
+                    }
+                ]
+            }
+        },
+        {
+            $project:{
+                _id:false, name:true, lastName:true
+            }
+        },
+        {
+            $project:{
+                fullName:{
+                    $concat: ['$name',' ','$lastName']
+                }
+            }
+        }
+    ]
+)
